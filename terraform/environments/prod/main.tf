244623757d28af76471dd0b9a4a900577ba5e975
job ueno <job.ueno.2024@gmail.com>
2025-07-09 00:26:29 +0900

    Add IAM member for public Cloud Run access and update module outputs
    - Add a resource to grant "run.invoker" role to allUsers for internal Cloud Run service.
    - Rename and add outputs for improved clarity in module configuration.

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -11,3 +11,10 @@ module "internal_cloudrun_service" {
   image    = "gcr.io/cloudrun/hello"
   ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
 }
+
+resource "google_cloud_run_v2_service_iam_member" "no_auth" {
+  location = module.internal_cloudrun_service.location
+  name     = module.internal_cloudrun_service.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
--- terraform/modules/cloudrun/outputs.tf
+++ terraform/modules/cloudrun/outputs.tf
@@ -1,11 +1,16 @@
 # modules/google_cloud_run_v2_service/outputs.tf
 
-output "service_name" {
+output "name" {
   description = "The name of the created Cloud Run service."
   value       = google_cloud_run_v2_service.service.name
 }
 
-output "service_uri" {
+output "location" {
+  description = "The location of the created Cloud Run service."
+  value       = google_cloud_run_v2_service.service.location
+}
+
+output "uri" {
   description = "The URI of the created Cloud Run service."
   value       = google_cloud_run_v2_service.service.uri
 }
